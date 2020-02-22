0658198cd7a201d89a676d32d6b8fb9e58a5a574
erinroll <erollestone@gmail.com>
2020-02-22 17:28:58 +1100

    ip location working

--- theweather/urls.py
+++ theweather/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path, include
+from django.conf import settings
+from weather import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('weather.urls')),
+    re_path(r'^get_weather_from_ip/', views.get_weather_from_ip, name='get_weather_from_ip'),
 ]
